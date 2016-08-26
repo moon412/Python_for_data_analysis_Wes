e69bcc4b0d5adb4dc7066c5599637d1dcb319a4f
yue <moonblue412@gmail.com>
2016-08-26 07:56:04 -0500

    update chapter 8

--- chapter8.py
+++ chapter8.py
@@ -69,3 +69,135 @@ ax.plot(np.random.randn(1000).cumsum(), 'k', label='one')
 ax.plot(np.random.randn(1000).cumsum(), 'k--', label='two')
 ax.plot(np.random.randn(1000).cumsum(), 'k.', label='three')
 ax.legend(loc='best')
+
+"""
+Annotations and Drawing on a Subplot
+"""
+import os
+os.chdir('D:\courses\Python_for_data_analysis')
+from datetime import datetime
+import pandas as pd
+data = pd.read_csv('ch08/spx.csv', index_col=0, parse_dates=True)
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+spx = data['SPX']
+spx.plot(ax=ax, style='k')
+ax.set_xlim(['1/1/2007', '1/1/2011'])
+ax.set_ylim([600, 1800])
+ax.set_title('Important dates in 2008-2009 financial crisis')
+crisis_data = [(datetime(2007, 10, 11), 'Peak of bull market'),
+               (datetime(2008, 3, 12), 'Bear Stearns Fails'),
+               (datetime(2008, 9, 15), 'Lehman Bankruptcy')]
+
+for date, label in crisis_data:
+    ax.annotate(label, xy=(date, spx.asof(date) + 100),
+                xytext=(date, spx.asof(date) + 200),
+                arrowprops=dict(facecolor='black'),
+                horizontalalignment='left', verticalalignment='top')
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+rect = plt.Rectangle((0.2, 0.75), 0.4, 0.15, color='k', alpha=0.3)
+circ = plt.Circle((0.7, 0.2), 0.15, color='b', alpha=0.3)
+pgon = plt.Polygon([[0.15, 0.15], [0.35, 0.4], [0.2, 0.6]],
+                   color='g', alpha=0.5)
+ax.add_patch(rect)
+ax.add_patch(circ)
+ax.add_patch(pgon)
+
+"""
+Plotting Functions in pandas
+"""
+"""
+Line plots
+"""
+s = pd.Series(np.random.randn(10).cumsum(), index=np.arange(0, 100, 10))
+s.plot()
+
+df = pd.DataFrame(np.random.randn(10, 4).cumsum(0),
+               columns=['A', 'B', 'C', 'D'],
+               index=np.arange(0, 100, 10))
+df.plot()    
+
+"""
+Bar plots
+"""
+fig, axes = plt.subplots(2, 1)
+data = pd.Series(np.random.rand(16), index=list('abcdefghijklmnop'))
+data.plot(kind='bar', ax=axes[0], color='k', alpha=0.7)
+data.plot(kind='barh', ax=axes[1], color='k', alpha=0.7)
+df = pd.DataFrame(np.random.rand(6, 4),
+                  index=['one', 'two', 'three', 'four', 'five', 'six'],
+                  columns=pd.Index(['A', 'B', 'C', 'D'], name='Genus'))
+df.plot(kind='bar')
+df.plot(kind='barh', stacked=True)
+tips = pd.read_csv('ch08/tips.csv')
+party_counts = pd.crosstab(index=tips.day, columns=tips['size'])           
+party_counts = party_counts.ix[:, 2:5]
+party_pcts = party_counts.div(party_counts.sum(1).astype(float), axis=0)
+#pandas.DataFrame.sum(axis) sum along which axis, sum the numbers on which axis
+#pandas.DataFrame.divide(axis): divide the number along wich axis
+party_counts.divide([10,100,1000,10000], axis=0)               
+party_pcts.plot(kind='bar', stacked=True)   
+
+"""
+Histograms and Density Plots
+"""
+tips['tip_pct'] = tips['tip']/tips['total_bill']      
+tips['tip_pct'].hist(bins=50)    
+tips['tip_pct'].plot(kind='hist', bins=50)      
+tips['tip_pct'].plot(kind='kde')
+comp1 = np.random.normal(0, 1, size=200) #N(0, 1)
+comp2 = np.random.normal(10, 2, size=200) #N(10, 4)
+values = pd.Series(np.concatenate([comp1, comp2]))
+values.hist(bins=100, alphs=0.3, color='k', normed=True)
+values.plot(kind='kde', style='k--')
+"""
+Scatter Plots
+"""
+macro = pd.read_csv('ch08/macrodata.csv')
+data = macro[['cpi', 'm1', 'tbilrate', 'unemp']]
+trans_data = np.log(data).diff().dropna()
+plt.scatter(trans_data['m1'], trans_data['unemp'])
+plt.title('Changes in log %s vs. log %s' % ('m1', 'unemp'))
+pd.scatter_matrix(trans_data, diagonal='kde', color='k', alpha=0.3)
+"""
+Plotting Maps: Visualizing Haiti Earthquake Crisis Data
+"""
+data = pd.read_csv('ch08/Haiti.csv')
+data[['INCIDENT DATE', 'LATITUDE', 'LONGITUDE']][:10]
+data['CATEGORY'][:6]
+data = data[(data.LATITUDE > 18) & (data.LATITUDE < 20) &
+            (data.LONGITUDE > -75) & (data.LONGITUDE < -70) &
+            data.CATEGORY.notnull()]
+def get_english(cat):
+    """
+    cat is one category
+    return the code and English content
+    """
+    code, names = cat.split('.')
+    if '|' in names:
+        names = names.split('|')[1]
+    return code, names.strip()
+    
+def to_cat_list(catstr):
+    stripped = (x.strip() for x in catstr.split(','))
+    return [x for x in stripped if x]
+    
+def get_all_categories(cat_series):
+    cat_sets = (set(to_cat_list(x)) for x in cat_series)
+    return sorted(set.union(*cat_sets))
+ 
+all_cats = get_all_categories(data.CATEGORY)
+english_mapping = dict(get_english(x) for x in all_cats)
+
+def get_code(seq):
+    return [x.split('.')[0] for x in seq if x]
+all_codes = get_code(all_cats)
+import numpy as np
+code_index = pd.Index(np.unique(all_codes))
+dummy_frame = pd.DataFrame(np.zeros((len(data), len(code_index))), 
+                           index=data.index, columns=code_index)   
+for row, cat in zip(data.index, data.CATEGORY):
+    codes = get_code(to_cat_list(cat))
+    dummy_frame.ix[row, codes] = 1
+    
