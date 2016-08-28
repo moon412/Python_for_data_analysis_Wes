905f353b31b6366ce77b7eb9ae013e55d50ed0a6
yue <moonblue412@gmail.com>
2016-08-28 10:39:36 -0500

    update chapter 09

--- chapter9.py
+++ chapter9.py
@@ -7,6 +7,9 @@ Chapter 9 Data Aggregation and Group Operations
 """
 import pandas as pd
 import numpy as np
+"""
+Groupby Mechanics
+"""
 df = pd.DataFrame({'key1': ['a', 'a', 'b', 'b', 'a'],
                 'key2': ['one', 'two', 'one', 'two', 'one'],
                 'data1': np.random.randn(5),
@@ -19,4 +22,69 @@ means.unstack()
 states = np.array(['Ohio', 'California', 'California', 'Ohio', 'Ohio'])
 years = np.array([2005, 2005, 2006, 2005, 2006])
 df['data1'].groupby([states, years]).mean()
+df.groupby('key1').mean()
+df.groupby(['key1', 'key2']).mean()
+df.groupby(['key1', 'key2']).size()
 
+"""
+Iterating Over Games
+"""
+for name, group in df.groupby('key1'):
+    print name
+    print group
+for (k1, k2), group in df.groupby(['key1', 'key2']):
+    print k1, k2
+    print group
+pieces = dict(list(df.groupby('key1')))
+list(df.groupby(['key1', 'key2']))
+grouped = df.groupby(df.dtypes, axis = 1)
+dict(list(grouped))
+
+"""
+Selecting a Column or Subset of Columns
+"""
+df.groupby('key1')['data1']
+df.groupby('key1')[['data1']]
+df['data1'].groupby(df['key1'])
+df[['data1']].groupby(df['key1'])
+df.groupby(['key1', 'key2'])[['data2']].mean()
+
+"""
+Grouping with Dicts and Series
+"""
+people = pd.DataFrame(np.random.randn(5, 5),
+                      columns=['a', 'b', 'c', 'd', 'e'],
+                      index=['Joe', 'Steve', 'Wes', 'Jim', 'Travis'])
+people.ix[2:3, ['b', 'c']] = np.nan
+mapping = {'a': 'red', 'b': 'red', 'c': 'blue',
+           'd': 'blue', 'e': 'red', 'f': 'orange'}
+by_column = people.groupby(mapping, axis=1)
+by_column.sum()  
+people.groupby(['red', 'red', 'blue', 'blue', 'red'], axis=1).mean() 
+map_series = pd.Series(mapping)
+people.groupby(map_series, axis=1).count()
+
+"""
+Grouping with Functions
+"""
+people.groupby(len).sum()
+people.groupby(len, axis=1).sum()
+key_list = ['one', 'one', 'one', 'two', 'two']
+people.groupby([len, key_list]).min()
+
+"""
+Group by Index Levels
+"""
+columns = pd.MultiIndex.from_arrays([['US', 'US', 'US', 'JP', 'JP'],
+                                     [1, 3, 5, 1, 3]], names=['cty', 'tenor'])
+hier_df = pd.DataFrame(np.random.randn(4, 5), columns=columns)
+hier_df.groupby(level='cty', axis=1).count()
+
+"""
+Data Aggregation
+"""
+
+                                     
+
+
+        
